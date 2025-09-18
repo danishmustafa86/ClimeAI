7f67a8052b08b94ef0c64f3885250ea808f2cdf2
Muhammadzainattiq <zainatteeq@gmail.com>
2025-09-18 10:29:05 +0500

    updated llm to aiml one4

--- utils/llm.py
+++ utils/llm.py
@@ -5,6 +5,33 @@ from configurations.config import config
 
 load_dotenv()
 
+# To create the instance of Any LangChain Model
+# def get_llm_instance(
+#     temperature: float = 0.7,
+#     **kwargs
+# ):
+#     """
+#     Initialize and return a LangChain chat model instance for any provider,
+#     and allowing temperature and other parameters to be set.
+
+#     Args:
+#         temperature (float, optional): Sampling temperature for the model. Defaults to 0.7.
+#         **kwargs: Additional optional parameters to pass to the model constructor.
+
+#     Returns:
+#         An instance of the chat model.
+#     """
+
+#     # Initialize the chat model with temperature and any other kwargs
+#     model = init_chat_model(
+#         config.MODEL_NAME,
+#         model_provider=config.MODEL_PROVIDER,
+#         temperature=temperature,
+#         **kwargs
+#     )
+#     return model
+
+# To create the instance of AIMl Model
 def get_llm_instance(
     temperature: float = 0.7,
     **kwargs
@@ -22,13 +49,16 @@ def get_llm_instance(
     """
 
     # Initialize the chat model with temperature and any other kwargs
-    model = init_chat_model(
-        config.MODEL_NAME,
-        model_provider=config.MODEL_PROVIDER,
+    from langchain_aimlapi import AimlapiLLM
+
+    model = AimlapiLLM(
+        model=config.MODEL_NAME,
         temperature=temperature,
+        api_key=config.AIML_API_KEY,
         **kwargs
     )
     return model
+
 
 def get_llm_instance_with_tools(
     temperature: float = 0.7,
