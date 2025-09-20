67cedd099d7a18fe72c806b6f499cbda671dafb9
Danish Mustafa <danishjajja86@gmail.com>
2025-09-20 13:42:49 -0700

    main.py issue solved

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routes.health_check_routes import health_check_router
 from routes.chat_routes import chat_router
 from routes.event_advisor_routes import event_advisor_router
 from routes.travel_advisor_routes import travel_advisor_router
@@ -8,6 +7,10 @@ import os
 
 app = FastAPI()
 
+
+@app.get("/")
+def index():
+    return "Welcome to the Backend API..."
 # Add CORS middleware
 
 # Get allowed origins from environment variable or use defaults
@@ -34,6 +37,5 @@ app.add_middleware(
 )
 
 app.include_router(chat_router)
-app.include_router(health_check_router)
 app.include_router(event_advisor_router)
 app.include_router(travel_advisor_router)
